2a472747da228008e2b2b8233afee6f26d6fba6a
Luis Araujo <luis.araujo@laureate.net>
2020-04-22 14:31:29 -0600

    When user inputs multiple number with new delimiters

--- src/Kata.Spec/when_feeding_the_monkey.cs
+++ src/Kata.Spec/when_feeding_the_monkey.cs
@@ -60,8 +60,32 @@ namespace Kata.Spec
     }
 
     // Given the user input is two numbers when calculating the sum then it should return the sum of those numbers. (example "1,2" should equal 3)
+    
     // Given the user input is an unknown amount of numbers when calculating the sum then it should return the sum of all the numbers. (example "1,2,3" should equal 6)
+    public class when_user_inputs_unknown_amount_of_numbers
+    {
+        Establish _context = () => { _systemUnderTest = new Calculator(); };
+
+        Because of = () => { _result = _systemUnderTest.Add("1,2,3"); };
+
+        It should_return_the_sum = () => { _result.Should().Be(6); };
+        private static Calculator _systemUnderTest;
+        private static int _result;
+    }
+
     // Given the user input is multiple numbers with new line and comma delimiters when calculating the sum then it should return the sum of all the numbers. (example "1\n2,3" should equal 6)
+
+    public class when_user_inputs_multiple_numbers_with_comma_and_new_line
+    {
+        Establish _context = () => { _systemUnderTest = new Calculator(); };
+
+        Because of = () => { _result = _systemUnderTest.Add("1\n2,3"); };
+
+        It should_return_the_sum = () => { _result.Should().Be(6); };
+        private static Calculator _systemUnderTest;
+        private static int _result;
+    }
+
     // Given the user input is multiple numbers with a custom single-character delimiter when calculating the sum then it should return the sum of all the numbers. (example “//;\n1;2” should return 3)
     // Given the user input contains one negative number when calculating the sum then it should throw an exception "negatives not allowed: x" (where x is the negative number).
     // Given the user input contains multiple negative numbers mixed with positive numbers when calculating the sum then it should throw an exception "negatives not allowed: x, y, z" (where x, y, z are only the negative numbers).
--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -1,15 +1,20 @@
+using System.Linq;
+
 namespace Kata.Spec
 {
     public class Calculator
     {
         public int Add(string input = "")
         {
+            
             if (input == "")
             {
                 return 0;
             }
+
+            var result = input.Split(',').Select(int.Parse);
             
-            return int.Parse(input);
+            return result.Sum();
         }
     }
 }
